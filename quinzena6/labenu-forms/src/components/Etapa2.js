8e65723d2e081c83c045aecf08e72e3b060e2afc
Maurilio-Thiago <mtfv2004@hotmail.com>
2020-10-29 22:02:34 -0300

    LabenuForms

---
+++ quinzena6/labenu-forms/src/components/Etapa2.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Etapa2 extends React.Component {
+    render() {
+        return (
+            <div>
+                <h3><strong>ETAPA 2 - INFORMAÇÕES DO ENSINO SUPERIOR</strong></h3>
+                <p>5. Qual curso?</p>
+                <input></input>
+                <p>6. Qual a unidade de ensino?</p>
+                <input></input>
+                <br/>
+            </div>
+        )
+    }
+}
+
+export default Etapa2;
---
+++ quinzena6/labenu-forms/src/components/EtapaFinal.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class EtapaFinal extends React.Component {
+    render() {
+        return (
+            <div>
+                <h3><strong>O FORMULÁRIO ACABOU</strong></h3>
+                <p>Muito obrigado por participar! Entraremos em contato!</p>
+            </div>
+        )
+    }
+}
+
+export default EtapaFinal;
